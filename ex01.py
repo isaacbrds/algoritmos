b37149ced35ac0f022d8729ee912f4d6dadcee63
isaacbrds <isaacbrds>
2022-06-20 18:19:08 -0300

    Ex 01

---
+++ ex01.py
@@ -0,0 +1,5 @@
+temperatura_celsius = int(input('Digite a temperatura a ser convertida: '))
+
+temperatura_farenheit = ( temperatura_celsius * 9 ) / 5  + 32
+print(f'temperatura em farenheit é : {temperatura_farenheit}')
+
