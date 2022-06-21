fc0ce93b4729ec5d04f72e004a6f4d80512872dc
isaacbrds <isaacbrds>
2022-06-21 13:29:44 -0300

    Ex 02

---
+++ ex02.py
@@ -0,0 +1,12 @@
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite um número: '))
+numero_3 = float(input('Digite um número: '))
+
+resultado = ( 2 * numero_1) * (numero_2 / 2)
+print(f'O produto do dobro do primeiro pela metade do segundo é: {resultado}')
+
+resultado = 3 * numero_1 + numero_3
+print(f'A soma do triplo do primeiro com o terceiro é: {resultado}')
+
+resultado = numero_3 ** 3
+print(f'O terceiro elevado ao cubo é: {resultado}')
