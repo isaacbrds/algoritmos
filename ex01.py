cb236e78df4f38c3daac8bdbf3a55ed34b73b41e
Isaac Brigido <isaac.brigido@gmail.com>
2022-10-24 18:51:58 -0300

    Delete ex01.py

--- ex01.py
+++
@@ -1,5 +0,0 @@
-temperatura_celsius = int(input('Digite a temperatura a ser convertida: '))
-
-temperatura_farenheit = ( temperatura_celsius * 9 ) / 5  + 32
-print(f'temperatura em farenheit é : {temperatura_farenheit}')
-
